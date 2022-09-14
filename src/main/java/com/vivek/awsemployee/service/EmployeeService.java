1f27a9238f9da74ec300f6fbc3b7a825e9d4e370
vivek <vivek.dubeydeveloper@gmail.com>
2022-09-14 23:16:46 +0530

    update code

--- src/main/java/com/vivek/awsemployee/service/EmployeeService.java
+++ src/main/java/com/vivek/awsemployee/service/EmployeeService.java
@@ -13,7 +13,7 @@ public class EmployeeService {
 	public List<Employee> getEmployee() {
 
 		return Arrays.asList(new Employee(1, "Vivek", 5000), new Employee(2, "Pankaj", 8000),
-				new Employee(3, "Mahendra", 5000));
+				new Employee(3, "Mahendra", 5000),new Employee(4, "Mahi", 15000),new Employee(5, "sachin", 17000));
 	}
 
 }
